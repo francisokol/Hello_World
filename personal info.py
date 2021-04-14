d92c5dad321fbcae58bf86dcf3ed180d85c23a18
francisokol <82500975+francisokol@users.noreply.github.com>
2021-04-14 16:03:59 +0800

    Add files via upload

---
+++ personal info.py
@@ -0,0 +1,10 @@
+firstname = str(input("What is your first name? "))
+lastname = str(input("What is your last name? "))
+location = str(input("In what country do you live? "))
+age = input("How old are you? ")
+space = " "
+print("Hello my name is" + space + firstname + space + lastname + space + "and I live in the" + space + location + space + "I am" + space + age + space + "years old.")
+
+      
+
+    
